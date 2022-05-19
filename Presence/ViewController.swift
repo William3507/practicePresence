05e8676897ef8bd15129ce7cec2f55342c4cc281
williambailey <bailwj20@wfu.edu>
2022-05-19 13:49:49 -0400

    Initial Commit

---
+++ Presence/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Presence
+//
+//  Created by William Bailey on 5/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
